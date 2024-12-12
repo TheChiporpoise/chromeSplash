f318822588ea0f6051654ed89d911af84d5f55ae
TheChiporpoise <thechiporpoise@gmail.com>
2024-12-11 17:08:50 -0700

    fixed reload on submit so the web element could be moved down

--- scripts/page2.js
+++ scripts/page2.js
@@ -17,25 +17,34 @@ function textList_format(text) {
     return out;
 }
 
-
 var text;
-if (localStorage.getItem("test_text")) {
-    text = localStorage.getItem("test_text");
-} else {
-    text = "up to 20 characters";
-    localStorage.setItem("test_text", text);
+var welcomeText, wText_Store;
+function textSet() {
+    if (localStorage.getItem("test_text")) {
+        text = localStorage.getItem("test_text");
+    } else {
+        text = "up to 20 characters";
+        localStorage.setItem("test_text", text);
+    }
+    welcomeText = "_", wText_Store = textList_format(text);
 }
-
-var welcomeText = "_", wText_Store = textList_format(text);
+textSet();
 
 function updateText() {
     welcomeText = wText_Store[welcomeText.length - 1];
     document.getElementById("test_output").innerHTML = welcomeText;
 }
 
-console.log(wText_Store);
-// console.log(wText_Store.length * 4);
+function reset() {
+    document.getElementById("test_output").innerHTML = "_";
+    document.getElementById("test_input").innerText = "";
+}
+
 function draw() {
+    if (frameCount < 15) {
+        reset();
+        textSet();
+    }
     if (frameCount < 15 + (wText_Store.length * 4)) {
         if (frameCount % 4 == 0 && frameCount > 15) {
             updateText();
